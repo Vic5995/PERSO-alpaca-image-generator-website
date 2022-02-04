27b4b3cb515da873b9cb4d65177287fdfa7d86d1
Victoire <victoire.lenglart.5995@gmail.com>
2022-02-04 14:55:07 +0100

    🚧 app.js but issues with z-index

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,45 @@
-let ckCatergories = document.querySelectorAll(".categories-container input")
+// const imagesLabels = require("./ressources-labels");
+import {imagesLabels} from "./ressources-labels.js";
 
-let curCategory = "Background"
+const styleContainer = document.querySelector(".style-container")
 
-ckCatergories.forEach(el => {
-    el.addEventListener('change', () => {
-        ckCatergories.forEach(el => {
-            if (el.checked) {
-                curCategory = el.value;
-                displayStyle();
-            }
-        })
-    })
+imagesLabels.forEach( label => {
+    let el = `<div id=${label.category}-style class="style-container-list hidden">\n`
+    for (let i = 0; i < label.items.length; i++) {
+        const item = label.items[i]
+        const l = label.labels[i]
+        el += `<input type="radio" name="${label.category}" id="${label.category}-${item}" value="${item}" hidden>\n<label class="ck-button" for="${label.category}-${item}">${l}</label>\n`
+    }
+    el += "</div>"
+    styleContainer.innerHTML += el
 })
 
-function displayStyle() {
+const styleContainerLists = document.querySelectorAll(".style-container-list")
 
+const ckCategories = document.querySelectorAll(".categories-container input")
+let curCategory = ""
+
+ckCategories.forEach( el => el.addEventListener('change', event => {
+    if (el.checked) {
+        if (curCategory !== "") {
+            let styleList = document.querySelector(`#${curCategory.toLowerCase()}-style`)
+            styleList.classList.add("hidden")
+        }
+        curCategory = el.value;
+        let styleList = document.querySelector(`#${curCategory.toLowerCase()}-style`)
+        styleList.classList.remove("hidden")
+    }
+}))
+
+for (let i = 0; i < styleContainerLists.length; i++) {
+    const container = styleContainerLists[i]
+    const inputs = container.querySelectorAll("input")
+    const imgContainer = document.querySelector(`.img-container img:nth-child(${i + 1})`)
+    inputs.forEach( input => {
+        input.addEventListener('change', () => {
+            const loc = input.id.split("-")
+            imgContainer.src = `res/images/${loc[0]}/${loc[1]}.png`
+        })
+    })
 }
+
---
+++ dist/js/ressources-labels.js
@@ -0,0 +1,165 @@
+export const imagesLabels = [
+    {
+        "category": "accessories",
+        "items": [
+            "earings",
+            "flower",
+            "glasses",
+            "headphone"
+        ],
+        "labels": [
+            "Earings",
+            "Flower",
+            "Glasses",
+            "Headphone"
+        ]
+    },
+    {
+        "category": "backgrounds",
+        "items" : [
+            "blue50",
+            "blue60",
+            "blue70",
+            "darkblue30",
+            "darkblue50",
+            "darkblue70",
+            "green50",
+            "green60",
+            "green70",
+            "grey40",
+            "grey70",
+            "grey80",
+            "red50",
+            "red60",
+            "red70",
+            "yellow50",
+            "yellow60",
+            "yellow70"
+        ],
+        "labels": [
+            "Blue 50",
+            "Blue 60",
+            "Blue 70",
+            "Darkblue 30",
+            "Darkblue 50",
+            "Darkblue 70",
+            "Green 50",
+            "Green 60",
+            "Green 70",
+            "Grey 40",
+            "Grey 70",
+            "Grey 80",
+            "Red 50",
+            "Red 60",
+            "Red 70",
+            "Yellow 50",
+            "Yellow 60",
+            "Yellow 70"
+        ]
+    },
+    {
+        "category": "ears",
+        "items": [
+            "default",
+            "tiltbackward",
+            "tiltforward"
+        ],
+        "labels": [
+            "Default",
+            "Tilt Backward",
+            "Tilt Forward"
+        ]
+    },
+    {
+        "category": "eyes",
+        "items": [
+            "default",
+            "angry",
+            "naughty",
+            "panda",
+            "smart",
+            "star"
+        ],
+        "labels": [
+            "Default",
+            "Angry",
+            "Naughty",
+            "Panda",
+            "Smart",
+            "Star"
+        ]
+    },
+    {
+        "category": "hair",
+        "items": [
+            "default",
+            "bang",
+            "curls",
+            "elegant",
+            "fancy",
+            "quiff",
+            "short"
+        ],
+        "labels": [
+            "Default",
+            "Bang",
+            "Curls",
+            "Elegant",
+            "Fancy",
+            "Quiff",
+            "Short"
+        ]
+    },
+    {
+        "category": "leg",
+        "items": [
+            "default",
+            "bubbletea",
+            "cookie",
+            "gameconsole",
+            "tiltbackward",
+            "tiltforward"
+        ],
+        "labels": [
+            "Default",
+            "Bubble Tea",
+            "Cookie",
+            "Game Console",
+            "Tilt Backward",
+            "Tilt Forward"
+        ]
+    },
+    {
+        "category": "mouth",
+        "items": [
+            "default",
+            "astonished",
+            "eating",
+            "laugh",
+            "tongue"
+        ],
+        "labels": [
+            "Default",
+            "Astonished",
+            "Eating",
+            "Laugh",
+            "Tongue"
+        ]
+    },
+    {
+        "category": "neck",
+        "items": [
+            "default",
+            "thick",
+            "bendbackward",
+            "bendforward"
+        ],
+        "labels": [
+            "Default",
+            "Thick",
+            "Bend Backward",
+            "Bend Forward"
+        ]
+    },
+]
+
