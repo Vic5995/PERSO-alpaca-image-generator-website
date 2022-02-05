65f6cdade2387240a275c6d22e6ad844f78a9d02
Victoire <victoire.lenglart.5995@gmail.com>
2022-02-05 19:43:59 +0100

    Updates

---
+++ js/app.js
@@ -0,0 +1,101 @@
+import mergeImages from './merge-images.js'
+
+import {imagesLabels} from "./ressources-labels.js";
+
+const NOSE_ID = 5
+
+const styleContainer = document.querySelector(".style-container")
+
+imagesLabels.forEach( label => {
+    let el = `<div id=${label.category}-style class="style-container-list hidden">\n`
+    for (let i = 0; i < label.items.length; i++) {
+        const item = label.items[i]
+        const l = label.labels[i]
+        el += `<input type="radio" name="${label.category}" id="${label.category}-${item}" value="${item}" hidden>\n<label class="ck-button" for="${label.category}-${item}">${l}</label>\n`
+    }
+    el += "</div>"
+    styleContainer.innerHTML += el
+})
+
+const styleContainerLists = document.querySelectorAll(".style-container-list")
+styleContainerLists.forEach( (el) => {
+    if (el.children.item(0) !== null) el.children.item(0).setAttribute("checked", "")
+})
+
+const ckCategories = document.querySelectorAll(".categories-container input")
+let curCategory = ""
+
+ckCategories.forEach( el => el.addEventListener('change', () => {
+    if (el.checked) {
+        if (curCategory !== "") {
+            let styleList = document.querySelector(`#${curCategory.toLowerCase()}-style`)
+            styleList.classList.add("hidden")
+        }
+        curCategory = el.value;
+        let styleList = document.querySelector(`#${curCategory.toLowerCase()}-style`)
+        styleList.classList.remove("hidden")
+    }
+}))
+
+for (let i = 0; i < styleContainerLists.length; i++) {
+    const container = styleContainerLists[i]
+    const inputs = container.querySelectorAll("input")
+    const imgContainer = document.querySelector(`.img-container .shifting-image:nth-child(${i + 1})`)
+    inputs.forEach( input => {
+        input.addEventListener('change', () => {
+            const loc = input.id.split("-")
+            if (loc[0] === "accessories") {
+                if (loc[1] !== "none") {
+                    imgContainer.src = `res/images/${loc[0]}/${loc[1]}.png`
+                    imgContainer.removeAttribute("hidden")
+                } else {
+                    imgContainer.src = "#"
+                    imgContainer.setAttribute("hidden", "")
+                }
+            } else {
+                imgContainer.src = `res/images/${loc[0]}/${loc[1]}.png`
+            }
+        })
+    })
+}
+
+const btnRandom = document.querySelector("#btn-random")
+
+btnRandom.addEventListener('click', () => {
+    imagesLabels.forEach(image => {
+        if(image.id !== NOSE_ID) {
+            const imgContainer = document.querySelector(`.img-container .shifting-image:nth-child(${image.id})`)
+            const r = Math.floor(Math.random() * image.items.length)
+            if (image.category === "accessories") {
+                if (r === 0) {
+                    imgContainer.src = "#"
+                    imgContainer.setAttribute("hidden", "")
+                } else {
+                    imgContainer.src = `res/images/${image.category}/${image.items[r]}.png`
+                    imgContainer.removeAttribute("hidden")
+                }
+            } else {
+                imgContainer.src = `res/images/${image.category}/${image.items[r]}.png`
+            }
+        }
+    })
+})
+
+const btnDownload = document.querySelector("#btn-download")
+
+btnDownload.addEventListener('click', () => {
+    const images = document.querySelectorAll(".img-container img")
+    let imageUrls = []
+    images.forEach( el => {
+        if (!el.hasAttribute("hidden")) {
+            imageUrls.push(el.src)
+        }
+    })
+    mergeImages(imageUrls).then( (b64) => {
+        let link = document.createElement("a")
+        link.download = 'alpaca.png'
+        link.href = b64;
+        link.click()
+    })
+})
+
---
+++ js/ressources-labels.js
@@ -0,0 +1,181 @@
+export const imagesLabels = [
+    {
+        "id": 1,
+        "category": "backgrounds",
+        "items" : [
+            "blue50",
+            "blue60",
+            "blue70",
+            "darkblue30",
+            "darkblue50",
+            "darkblue70",
+            "green50",
+            "green60",
+            "green70",
+            "grey40",
+            "grey70",
+            "grey80",
+            "red50",
+            "red60",
+            "red70",
+            "yellow50",
+            "yellow60",
+            "yellow70"
+        ],
+        "labels": [
+            "Blue 50",
+            "Blue 60",
+            "Blue 70",
+            "Darkblue 30",
+            "Darkblue 50",
+            "Darkblue 70",
+            "Green 50",
+            "Green 60",
+            "Green 70",
+            "Grey 40",
+            "Grey 70",
+            "Grey 80",
+            "Red 50",
+            "Red 60",
+            "Red 70",
+            "Yellow 50",
+            "Yellow 60",
+            "Yellow 70"
+        ]
+    },
+    {
+        "id": 2,
+        "category": "leg",
+        "items": [
+            "default",
+            "bubbletea",
+            "cookie",
+            "gameconsole",
+            "tiltbackward",
+            "tiltforward"
+        ],
+        "labels": [
+            "Default",
+            "Bubble Tea",
+            "Cookie",
+            "Game Console",
+            "Tilt Backward",
+            "Tilt Forward"
+        ]
+    },
+    {
+        "id": 3,
+        "category": "neck",
+        "items": [
+            "default",
+            "thick",
+            "bendbackward",
+            "bendforward"
+        ],
+        "labels": [
+            "Default",
+            "Thick",
+            "Bend Backward",
+            "Bend Forward"
+        ]
+    },
+    {
+        "id": 4,
+        "category": "ears",
+        "items": [
+            "default",
+            "tiltbackward",
+            "tiltforward"
+        ],
+        "labels": [
+            "Default",
+            "Tilt Backward",
+            "Tilt Forward"
+        ]
+    },
+    {
+        "id": 5,
+        "category": "nose",
+        "items": [],
+        "labels": []
+    },
+    {
+        "id": 6,
+        "category": "hair",
+        "items": [
+            "default",
+            "bang",
+            "curls",
+            "elegant",
+            "fancy",
+            "quiff",
+            "short"
+        ],
+        "labels": [
+            "Default",
+            "Bang",
+            "Curls",
+            "Elegant",
+            "Fancy",
+            "Quiff",
+            "Short"
+        ]
+    },
+    {
+        "id": 7,
+        "category": "eyes",
+        "items": [
+            "default",
+            "angry",
+            "naughty",
+            "panda",
+            "smart",
+            "star"
+        ],
+        "labels": [
+            "Default",
+            "Angry",
+            "Naughty",
+            "Panda",
+            "Smart",
+            "Star"
+        ]
+    },
+    {
+        "id": 8,
+        "category": "mouth",
+        "items": [
+            "default",
+            "astonished",
+            "eating",
+            "laugh",
+            "tongue"
+        ],
+        "labels": [
+            "Default",
+            "Astonished",
+            "Eating",
+            "Laugh",
+            "Tongue"
+        ]
+    },
+    {
+        "id": 9,
+        "category": "accessories",
+        "items": [
+            "none",
+            "earings",
+            "flower",
+            "glasses",
+            "headphone"
+        ],
+        "labels": [
+            "None",
+            "Earings",
+            "Flower",
+            "Glasses",
+            "Headphone"
+        ]
+    },
+]
+
